f4632408a88d1dd9fbfb58a07dda19d160234ec2
nheron <nicolas.heron.java@gmail.com>
2012-02-06 13:36:03 +0100

    Add Static method for guvnor connection

--- drools-framework-runtime-base/src/main/java/org/chtijbug/drools/runtime/resource/GuvnorDroolsResource.java
+++ drools-framework-runtime-base/src/main/java/org/chtijbug/drools/runtime/resource/GuvnorDroolsResource.java
@@ -1,11 +1,7 @@
 /**
- * 
+ *
  */
 package org.chtijbug.drools.runtime.resource;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 
 import org.drools.builder.ResourceType;
 import org.drools.io.Resource;
@@ -13,100 +9,116 @@ import org.drools.io.ResourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 /**
  * @author Bertrand Gressier
  * @date 9 déc. 2011
- * 
- * 
  */
 public class GuvnorDroolsResource implements DroolsResource {
 
-	static final Logger LOGGER = LoggerFactory.getLogger(GuvnorDroolsResource.class);
+    static final Logger LOGGER = LoggerFactory.getLogger(GuvnorDroolsResource.class);
 
-	private final String guvnor_url;
-	private String guvnor_appName = "drools-guvnor";
-	private final String guvnor_packageName;
-	private String guvnor_packageVersion = "/LATEST";
-	private final String guvnor_username;
-	private final String guvnor_password;
+    private final String guvnor_url;
+    private String guvnor_appName = "drools-guvnor";
+    private final String guvnor_packageName;
+    private String guvnor_packageVersion = "/LATEST";
+    private final String guvnor_username;
+    private final String guvnor_password;
 
-	private Resource changesetResource;
+    private Resource changesetResource;
 
-	public GuvnorDroolsResource(String guvnor_url, String guvnor_packageName, String guvnor_username, String guvnor_password) {
-		this.guvnor_url = guvnor_url;
-		this.guvnor_packageName = guvnor_packageName;
-		this.guvnor_username = guvnor_username;
-		this.guvnor_password = guvnor_password;
+    public static GuvnorDroolsResource createGuvnorRessource(String guvnor_url, String guvnor_packageName, String guvnor_username, String guvnor_password) {
+        return new GuvnorDroolsResource(guvnor_url, guvnor_packageName, guvnor_username, guvnor_password);
 
-	}
+    }
 
-	public GuvnorDroolsResource(String guvnor_url, String guvnor_appName, String guvnor_packageName, String guvnor_username, String guvnor_password) {
-		this(guvnor_url, guvnor_packageName, guvnor_username, guvnor_password);
-		this.guvnor_appName = guvnor_appName;
-	}
+    public GuvnorDroolsResource(String guvnor_url, String guvnor_packageName, String guvnor_username, String guvnor_password) {
+        this.guvnor_url = guvnor_url;
+        this.guvnor_packageName = guvnor_packageName;
+        this.guvnor_username = guvnor_username;
+        this.guvnor_password = guvnor_password;
 
-	public GuvnorDroolsResource(String guvnor_url, String guvnor_appName, String guvnor_packageName, String guvnor_packageVersion, String guvnor_username, String guvnor_password) {
-		this(guvnor_url, guvnor_appName, guvnor_packageVersion, guvnor_username, guvnor_password);
+    }
 
-		this.guvnor_packageVersion = guvnor_packageVersion;
-	}
+    public static GuvnorDroolsResource createGuvnorRessource(String guvnor_url, String guvnor_appName, String guvnor_packageName, String guvnor_username, String guvnor_password) {
+        return new GuvnorDroolsResource(guvnor_url, guvnor_appName, guvnor_packageName, guvnor_username, guvnor_password);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.chtijbug.drools.runtime.resource.DroolsResource#getResource()
-	 */
-	@Override
-	public Resource getResource() {
-		if (changesetResource != null) {
-			return changesetResource;
-		}
+    }
 
-		StringBuffer changesetxml = null;
+    public GuvnorDroolsResource(String guvnor_url, String guvnor_appName, String guvnor_packageName, String guvnor_username, String guvnor_password) {
+        this(guvnor_url, guvnor_packageName, guvnor_username, guvnor_password);
+        this.guvnor_appName = guvnor_appName;
+    }
 
-		StringBuilder buff = new StringBuilder();
-		buff.append(guvnor_url);
-		buff.append(guvnor_appName);
-		buff.append("/org.drools.guvnor.Guvnor/package/");
-		buff.append(guvnor_packageName);
-		buff.append(guvnor_packageVersion);
+    public static GuvnorDroolsResource createGuvnorRessource(String guvnor_url, String guvnor_appName, String guvnor_packageName, String guvnor_packageVersion, String guvnor_username, String guvnor_password) {
+        return new GuvnorDroolsResource(guvnor_url, guvnor_appName, guvnor_packageName, guvnor_packageVersion, guvnor_username, guvnor_password);
+    }
 
-		changesetxml = new StringBuffer();
-		changesetxml.append("<change-set xmlns='http://drools.org/drools-5.0/change-set' \n xmlns:xs='http://www.w3.org/2001/XMLSchema-instance'> \n  \n <add> \n");// xs:schemaLocation='http://drools.org/drools-5.0/change-set
-																																									// http://anonsvn.jboss.org/repos/labs/labs/jbossrules/trunk/drools-api/src/main/resources/change-set-1.0.0.xsd'
-		changesetxml.append("<resource source='");
-		changesetxml.append(buff.toString());
-		changesetxml.append("' type='PKG' basicAuthentication=\"enabled\" username=\"");
-		changesetxml.append(guvnor_username);
-		changesetxml.append("\" password=\"");
-		changesetxml.append(guvnor_password);
-		changesetxml.append("\" /> \n </add> \n </change-set>\n");
-		File fxml = null;
-		try {
+    public GuvnorDroolsResource(String guvnor_url, String guvnor_appName, String guvnor_packageName, String guvnor_packageVersion, String guvnor_username, String guvnor_password) {
+        this(guvnor_url, guvnor_appName, guvnor_packageVersion, guvnor_username, guvnor_password);
 
-			fxml = File.createTempFile("ChangeSet", ".xml");
-			fxml.deleteOnExit();
+        this.guvnor_packageVersion = guvnor_packageVersion;
+    }
 
-			BufferedWriter output = new BufferedWriter(new FileWriter(fxml));
-			output.write(changesetxml.toString());
-			output.close();
-		} catch (Exception e) {
-			LOGGER.error("Error creating file {}", fxml);
-		}
+    /*
+      * (non-Javadoc)
+      *
+      * @see org.chtijbug.drools.runtime.resource.DroolsResource#getResource()
+      */
+    @Override
+    public Resource getResource() {
+        if (changesetResource != null) {
+            return changesetResource;
+        }
 
-		changesetResource = ResourceFactory.newFileResource(fxml);
-		return changesetResource;
-	}
+        StringBuffer changesetxml = null;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.chtijbug.drools.runtime.resource.DroolsResource#getResourceType()
-	 */
-	@Override
-	public ResourceType getResourceType() {
+        StringBuilder buff = new StringBuilder();
+        buff.append(guvnor_url);
+        buff.append(guvnor_appName);
+        buff.append("/org.drools.guvnor.Guvnor/package/");
+        buff.append(guvnor_packageName);
+        buff.append(guvnor_packageVersion);
 
-		return ResourceType.CHANGE_SET;
-	}
+        changesetxml = new StringBuffer();
+        changesetxml.append("<change-set xmlns='http://drools.org/drools-5.0/change-set' \n xmlns:xs='http://www.w3.org/2001/XMLSchema-instance'> \n  \n <add> \n");// xs:schemaLocation='http://drools.org/drools-5.0/change-set
+        // http://anonsvn.jboss.org/repos/labs/labs/jbossrules/trunk/drools-api/src/main/resources/change-set-1.0.0.xsd'
+        changesetxml.append("<resource source='");
+        changesetxml.append(buff.toString());
+        changesetxml.append("' type='PKG' basicAuthentication=\"enabled\" username=\"");
+        changesetxml.append(guvnor_username);
+        changesetxml.append("\" password=\"");
+        changesetxml.append(guvnor_password);
+        changesetxml.append("\" /> \n </add> \n </change-set>\n");
+        File fxml = null;
+        try {
+
+            fxml = File.createTempFile("ChangeSet", ".xml");
+            fxml.deleteOnExit();
+
+            BufferedWriter output = new BufferedWriter(new FileWriter(fxml));
+            output.write(changesetxml.toString());
+            output.close();
+        } catch (Exception e) {
+            LOGGER.error("Error creating file {}", fxml);
+        }
+
+        changesetResource = ResourceFactory.newFileResource(fxml);
+        return changesetResource;
+    }
+
+    /*
+      * (non-Javadoc)
+      *
+      * @see
+      * org.chtijbug.drools.runtime.resource.DroolsResource#getResourceType()
+      */
+    @Override
+    public ResourceType getResourceType() {
+
+        return ResourceType.CHANGE_SET;
+    }
 }

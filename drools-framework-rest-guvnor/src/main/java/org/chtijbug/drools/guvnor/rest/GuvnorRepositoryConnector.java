bbf7844c9f859fd28afba9cf4e8b5c5b3a19b956
nheron <nicolas.heron.java@gmail.com>
2012-06-04 09:58:13 +0200

    DROOLS-3

--- drools-framework-rest-guvnor/src/main/java/org/chtijbug/drools/guvnor/rest/GuvnorRepositoryConnector.java
+++ drools-framework-rest-guvnor/src/main/java/org/chtijbug/drools/guvnor/rest/GuvnorRepositoryConnector.java
@@ -1,12 +1,15 @@
 package org.chtijbug.drools.guvnor.rest;
 
-import org.chtijbug.drools.guvnor.rest.dt.DecisionTable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.apache.cxf.common.util.Base64Utility;
+import org.apache.cxf.jaxrs.client.ClientConfiguration;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
+import org.chtijbug.drools.guvnor.rest.dt.DecisionTable;
 import org.drools.ide.common.client.modeldriven.dt52.GuidedDecisionTable52;
 import org.drools.ide.common.server.util.GuidedDTXMLPersistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,6 +24,8 @@ public class GuvnorRepositoryConnector implements RestRepositoryConnector{
     private String packageName;
     private String clientPath;
     private String authorizationHeader;
+    private long connectionTimeout=0;
+    private long receivedTimeout=0;
     private static final Logger LOGGER = LoggerFactory.getLogger(GuvnorRepositoryConnector.class);
 
     public GuvnorRepositoryConnector(String guvnorUrl,String guvnorAppName,String packageName,String guvnorUserName,String guvnorPassword) {
@@ -31,6 +36,22 @@ public class GuvnorRepositoryConnector implements RestRepositoryConnector{
         this.packageName = packageName;
         this.clientPath = this.guvnorAppName + "/rest/packages/" + this.packageName + "/assets/";
         this.authorizationHeader = "Basic " + Base64Utility.encode((guvnorUserName + ":" + guvnorPassword).getBytes());
+    }
+
+    public long getReceivedTimeout() {
+        return receivedTimeout;
+    }
+
+    public void setReceivedTimeout(long receivedTimeout) {
+        this.receivedTimeout = receivedTimeout;
+    }
+
+    public long getConnectionTimeout() {
+        return connectionTimeout;
+    }
+
+    public void setConnectionTimeout(long connectionTimeout) {
+        this.connectionTimeout = connectionTimeout;
     }
 
     @Override
@@ -48,6 +69,14 @@ public class GuvnorRepositoryConnector implements RestRepositoryConnector{
         String dtName = guidedDecisionTable52.getGuidedDecisionTable52().getTableName();
         String newContent = GuidedDTXMLPersistence.getInstance().marshal(guidedDecisionTable52.getGuidedDecisionTable52());
         WebClient client2 = WebClient.create(this.url);
+        ClientConfiguration config = WebClient.getConfig(client2);
+        HTTPConduit http = (HTTPConduit)config.getConduit();
+        HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
+        httpClientPolicy.setConnectionTimeout(connectionTimeout);
+        httpClientPolicy.setReceiveTimeout(receivedTimeout);
+
+        http.setClient(httpClientPolicy);
+
         client2.header("Authorization", this.authorizationHeader);
         client2.path(this.clientPath + dtName + "/source").accept("application/xml").put(newContent);
 

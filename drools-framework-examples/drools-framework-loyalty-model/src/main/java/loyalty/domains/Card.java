d89b5ad46e3897c88466562d99f652753b5d25f3
Nicolas Héron <nicolas.heron@pymma-software.com>
2016-05-03 09:53:21 +0200

    Branding the workbench to chtijbug part 1 : using Drools parent

---
+++ drools-framework-examples/drools-framework-loyalty-model/src/main/java/loyalty/domains/Card.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2014 Pymma Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package loyalty.domains;
+
+public class Card {
+    private String number;
+    private String name;
+
+    private Customer customer;
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void setCartType(String cartType) {
+        this.cartType = cartType;
+    }
+
+    private String cartType;
+
+    public Card(String number, String cartName, String cartType,
+                String customerID, String name, String surname, String maritalName,
+                Gender gender) {
+        this.number = number;
+        this.name = cartName;
+        this.cartType = cartType;
+        this.customer = new Customer(customerID, name, surname, maritalName,
+                gender, null);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCartType() {
+        return cartType;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Card() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
